f6a2216585761e47ccacc83e66c4660088c4d317
Hardi Jan <57617846+HardiJan@users.noreply.github.com>
2020-02-20 11:18:13 +0700

    Khởi tạo chương trình
    test

--- src/ap/src/main/app.java
+++ src/ap/src/main/app.java
@@ -1,5 +1,15 @@
+/**
+ * gói ap: định danh
+ * gói src: lưu code chương trình
+ * gói main: nơi chương trình bắt đầu
+ */
 package ap.src.main;
-
+/**
+ * class app: nơi chương trình bắt đầu
+ * * không tạo swing ở đây
+ */
 public class app {
-
+    public static void main(String args[]) {
+        System.out.println("Program has started");
+    }
 }
